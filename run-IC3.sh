2a5d4acc7ef63167f8c347807bebedf115aa07f0
navidseal <nsalehna@uci.edu>
2020-02-05 21:13:55 -0800

    Fix

--- run-IC3.sh
+++ run-IC3.sh
@@ -6,4 +6,4 @@ APK_NAME=${APK_NAME%.*}
 DARE_OUT=${2:-/dare_out/$APK_NAME}
 ANDROID_JAR=android-28.jar
 echo Run IC3 for $APK_NAME
-java -Xmx8g -jar ic3.jar -input $DARE_OUT -apkormanifest $APK_PATH -cp $ANDROID_JAR -protobuf $DARE_OUT &> $DARE_OUT/ic3.log
+java -Xmx8g -jar ic3.jar -input $DARE_OUT/retargeted/$APK_NAME -apkormanifest $APK_PATH -cp $ANDROID_JAR -protobuf $DARE_OUT -binary &> $DARE_OUT/ic3.log
